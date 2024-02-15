46300165e9d53c0c9fc87aeb2b8fa879c57a3be5
alfi87B <alfredoberca@gmail.com>
2024-02-15 19:35:36 +0100

    1er entrega curso ReactJs

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,16 @@
+import './ItemListContainer.css';
+import PropTypes from 'prop-types'; 
+
+const ItemListContainer = ({ greeting }) => {
+    return (
+        <div>
+            <h1>{greeting}</h1>
+        </div>
+    )
+}
+
+ItemListContainer.propTypes = {
+    greeting: PropTypes.string.isRequired // Espera que greeting sea una cadena y es requerido
+};
+
+export default ItemListContainer
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+const NavBar = () => {
+    return (
+        <nav className="NavBar">
+            <img src="/logo-ico.ico" alt="Zeppelint Cloth Logo" className="logo" />
+            {/* <h3> Zeppelint Cloth</h3> */}
+            <div>
+                <button>Remeras</button>
+                <button>Gorras</button>
+                <button>Buzos</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar 
